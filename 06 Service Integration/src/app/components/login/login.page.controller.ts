52f495d79d86853662433dce2fc3c97ea15f10ad
Braulio Diez Botella <brauliodiez@Braulios-MacBook-Pro.local>
2018-06-10 13:28:34 +0200

    06 almost there pending input bindings

--- 06 Service Integration/src/app/components/login/login.page.controller.ts
+++ 06 Service Integration/src/app/components/login/login.page.controller.ts
@@ -1,23 +1,23 @@
-import { LoginService } from '../../api/login'
+import { LoginService } from '../../api/login';
 
 export class LoginPageController {
   loginService: LoginService = null;
 
   constructor(LoginService: LoginService) {
-    "ngInject";
-
     this.loginService = LoginService;
   }
 
   doLogin(login: string, password: string) {
     this.loginService.validateLogin(login, password).then(
-       (succeeded) => {
-         if(succeeded) {
-            console.log('login succeeded');
-         } else {
-           console.log('login failed');
-         }
-       }
+      (succeeded) => {
+        if (succeeded) {
+          console.log('login succeeded');
+        } else {
+          console.log('login failed');
+        }
+      }
     );
   }
 }
+
+LoginPageController.$inject = ['LoginService'];
